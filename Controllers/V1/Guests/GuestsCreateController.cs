775c3c9bf902fbc1144730899a63cd61e1d6a723
jtorova5 <jtorova5@gmail.com>
2024-10-07 12:42:32 -0500

    feat: adding GuestsCreateController

---
+++ Controllers/V1/Guests/GuestsCreateController.cs
@@ -0,0 +1,43 @@
+using HotelAPI.DTOs.Requests;
+using HotelAPI.Models;
+using HotelAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelAPI.Controllers.V1.Guests;
+
+[ApiController]
+[Route("api/v1/guests")]
+public class GuestsCreateController : GuestsController
+{
+    public GuestsCreateController(IGuestRepository guestRepository) : base(guestRepository) { }
+
+    // POST: api/v1/guests
+    [HttpPost]
+    public async Task<IActionResult> AddGuest([FromBody] GuestDTO guestDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var guest = new Guest
+        {
+            FirstName = guestDto.FirstName,
+            LastName = guestDto.LastName,
+            Email = guestDto.Email,
+            IdentificationNumber = guestDto.IdentificationNumber,
+            PhoneNumber = guestDto.PhoneNumber,
+            Birthdate = guestDto.Birthdate
+        };
+
+        try
+        {
+            await _guestRepository.AddGuest(guest);
+            return Ok(guest);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Internal server error: {ex.Message}");
+        }
+    }
+}
