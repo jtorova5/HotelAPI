5e75fadf2a0f28f6f026dedd2a013157be62700c
jtorova5 <jtorova5@gmail.com>
2024-10-07 11:38:25 -0500

    feat: adding BookingsController

---
+++ Controllers/V1/Bookings/BookingsController.cs
@@ -0,0 +1,16 @@
+using HotelAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelAPI.Controllers.V1.Bookings;
+
+[ApiController]
+[Route("api/v1/bookings")]
+public class BookingsController : ControllerBase
+{
+    protected readonly IBookingRepository _bookingRepository;
+
+    public BookingsController(IBookingRepository bookingRepository)
+    {
+        _bookingRepository = bookingRepository;
+    }
+}
