2bc1462f44f64989ebac7e431676200de1db41cb
jtorova5 <jtorova5@gmail.com>
2024-10-07 07:29:17 -0500

    feat: adding Room model

---
+++ Models/Room.cs
@@ -0,0 +1,50 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelAPI.Models;
+
+[Table("rooms")]
+public class Room
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(10)]
+    [Column("room_number")]
+    public string? RoomNumber { get; set; }
+
+    [Required]
+    [Column("price_per_night")]
+    [Range(1, 10000)]
+    public double PricePerNight { get; set; }
+
+    [Required]
+    [Column("availability")]
+    public bool Availability { get; set; }
+
+    [Required]
+    [Column("max_occupancy_persons")]
+    [Range(1, 10)]
+    public byte MaxOccupancyPersons { get; set; }
+
+    [Required]
+    [Column("room_type_id")]
+    [ForeignKey("RoomTypeId")]
+    public int RoomTypeId { get; set; }
+
+    public Room(string roomNumber, double pricePerNight, bool availability, byte maxOccupancyPersons, int roomTypeId)
+    {
+        RoomNumber = roomNumber.Trim();
+        PricePerNight = pricePerNight;
+        Availability = availability;
+        MaxOccupancyPersons = maxOccupancyPersons;
+        RoomTypeId = roomTypeId;
+    }
+
+    public Room()
+    {
+    }
+}
